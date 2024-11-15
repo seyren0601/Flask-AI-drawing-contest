2d87c59cbca06fb239b95c404a2c7a9cf8aa730b
Thinh Nguyen <sorrenw@gmail.com>
2024-11-15 16:06:14 +0700

    Fix DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS ai_drawing_contest;
+CREATE DATABASE ai_drawing_contest;
 USE ai_drawing_contest;
 
 CREATE TABLE user(
@@ -8,14 +10,16 @@ CREATE TABLE user(
     group_id tinyint,
     salt nvarchar(128),
     hashed_pw nvarchar(256),
-    session_token text
-);
-
-CREATE TABLE participant(
-	user_id int,
+    session_token text,
     address text,
     date_of_birth date,
     register_date date,
     school varchar(128),
-    grade smallint
+    grade smallint,
+    PRIMARY KEY(user_id)
 );
+
+INSERT INTO user(name, email, group_id)
+VALUES 
+	("Thịnh Nguyễn", "sorrenw@gmail.com", 0),
+    ("abc", "abc@gmail.comm", 3)
