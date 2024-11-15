c241493810d1da9d602c3f9595831f3011ce694e
Minh Y <nguyenhominhy@gmail.com>
2024-11-15 21:33:00 +0700

    Define create_user()

--- Controllers/controller.py
+++ Controllers/controller.py
@@ -1,9 +1,13 @@
 from DB import db
-
+from Helper import user
 def get_all_user():
     users = db.GET_all_users()
     return users
 
 def create_user():
-    username 
-    password 
+    username = ''
+    password = user.random_password()
+    salt , hashed_pw = user.hash_password(password)
+
+    new_user = db.CREATE_user(username,salt,hashed_pw)
+    return new_user
