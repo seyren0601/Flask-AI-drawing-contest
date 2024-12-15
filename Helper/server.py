bea5315a91810468a39038f5006f9e116bf0d855
ThinhNguyen <sorrenw@gmail.com>
2024-12-15 11:00:10 +0700

    Update server token, .env file

--- Helper/server.py
+++ Helper/server.py
@@ -1,10 +1,15 @@
 import random
 import string
 import bcrypt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def server_authentication(bearer_token:str):
-    salt = b"$2b$12$QWtjgUEzesM0B4Kr2qqSeO"
-    hashed_pw = b"$2b$12$QWtjgUEzesM0B4Kr2qqSeOyd16C0CQIZAiNuyS1nzxLuA0lWpZney"
+    salt = os.environ["SERVER_SALT"].encode("utf-8")
+    hashed_pw = os.environ["SERVER_HASHED"].encode("utf-8")
 
     if bcrypt.hashpw(bearer_token.encode("utf-8"), salt) == hashed_pw:
         return True
