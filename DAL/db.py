3bbd66286dbcb8bb36ce8c3a11078110c032c8b4
Thinh Nguyen <sorrenw@gmail.com>
2024-12-27 11:25:59 +0700

    fix: Left join in /user endpoint (get user by group_id)

--- DAL/db.py
+++ DAL/db.py
@@ -195,16 +195,20 @@ def GET_user(user_id):
 def Get_user_by_group(group_id):
     with init_connection() as mysql_client:
         db_cursor = init_cursor(mysql_client)
-        query = f"""
-            SELECT *
-            FROM
-                (SELECT * FROM user WHERE group_id = {group_id}) AS A
-            INNER JOIN
-                (SELECT team_id, IF(SUM(submitted) > 0, true, false) AS submitted
-                FROM user INNER JOIN prompts ON user_id = team_id
-                GROUP BY team_id) AS B
-            ON A.user_id = B.team_id;
-        """
+        query = ""
+        if int(group_id) == 1:
+            query = f"""SELECT * FROM user WHERE group_id = 1;"""
+        else:
+            query = f"""
+                SELECT *
+                FROM
+                    (SELECT * FROM user WHERE group_id = 2) AS A
+                LEFT JOIN
+                    (SELECT team_id, IF(SUM(submitted) > 0, true, false) AS submitted
+                    FROM user INNER JOIN prompts ON user_id = team_id
+                    GROUP BY team_id) AS B
+                ON A.user_id = B.team_id;
+            """
         db_cursor.execute(query)
         res = db_cursor.fetchall()
         res = date_helper.query_date_to_string(res)
